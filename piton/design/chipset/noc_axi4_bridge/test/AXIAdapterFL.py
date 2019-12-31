51d6f1a7e604cf99997e5f94ebe113bccd242efd
yo96 <yo96@cornell.edu>
2019-12-31 13:17:22 -0500

    comments

--- piton/design/chipset/noc_axi4_bridge/test/AXIAdapterFL.py
+++ piton/design/chipset/noc_axi4_bridge/test/AXIAdapterFL.py
@@ -10,15 +10,39 @@ Author: Yanghui Ou
 '''
 from pymtl3 import *
 from pymtl3.stdlib.fl import MemoryFL
+from .piton_packet import *
 
 
 class AXIAdapterFL( Component ):
 
   def construct( s ):
+
+    s.mem = MemoryFL()
     ...
 
   def request( s, pkt ):
-    ...
+    if pkt.flits[0][ MTYPE ] == LOAD_MEM:
 
-  def response( s ):
-    ...
+      addr   = pkt.flits[0][ ADDR   ]
+      nbytes = size_map[ pkt.flits[0][ NBYTES ] ]
+      chipid = pkt.flits[0][ CHIPD ]
+      xpos   = pkt.flits[0][ XPOS  ]
+      ypos   = pkt.flits[0][ YPOS  ]
+      tag    = pkt.flits[0][ TAG   ]
+
+      # TODO:
+      # - support variable length
+      # - 64B aligned      
+      aligned_addr = 
+      data = s.mem.read( aligned_addr, 512 )
+
+      return mk_piton_rd_resp( data, tag, chipid, xpos, ypos )
+
+    elif pkt.flits[0][ MTYPE ] == NC_LOAD_REQ:
+      ...
+
+    elif pkt.flits[0][ MTYPE ] == STORE_MEM:
+      ...
+
+    elif pkt.flits[0][ MTYPE ] == NC_STORE_REQ:
+      ...
--- piton/design/chipset/noc_axi4_bridge/test/piton_packet.py
+++ piton/design/chipset/noc_axi4_bridge/test/piton_packet.py
@@ -28,13 +28,38 @@ RESV   = slice( 0,  6  )
 
 ADDR   = slice( 16, 64 )
 OPT2   = slice( 0,  16 )
+NBYTES = slice( 8,  11 )
 
 # FLIT3
 
 RESV3  = slice( 0, 30 )
 
 #-------------------------------------------------------------------------
-# PitonPacket
+# openpiton message type
+#-------------------------------------------------------------------------
+
+LOAD_MEM         = b8(19)
+NC_LOAD_REQ      = b8(14)
+LOAD_MEM_ACK     = b8(24)
+NC_LOAD_MEM_ACK  = b8(26)
+STORE_MEM        = b8(20)
+STORE_MEM_ACK    = b8(25)
+NC_STORE_REQ     = b8(15)
+NC_STORE_MEM_ACK = b8(27)
+
+size_map = {
+  b3( 0b000 ) : 0, 
+  b3( 0b001 ) : 1, 
+  b3( 0b010 ) : 2, 
+  b3( 0b011 ) : 4, 
+  b3( 0b100 ) : 8, 
+  b3( 0b101 ) : 16, 
+  b3( 0b110 ) : 32, 
+  b3( 0b111 ) : 64, 
+}
+
+#-------------------------------------------------------------------------
+# pitonpacket
 #-------------------------------------------------------------------------
 
 class PitonPacket:
@@ -42,3 +67,81 @@ class PitonPacket:
   def __init__( s, nflits ):
     s.flits = [ b64( 0 ) for _ in range( nflits ) ]
 
+#-------------------------------------------------------------------------
+# mk_piton_rd_req
+#-------------------------------------------------------------------------
+
+def mk_piton_rd_req( addr, nbytes, cacheable, tag, 
+                     dst_chipid=0, dst_x=0, dst_y=0, 
+                     src_chipid=0, src_x=0, src_y=0 ):  
+  pkt = PitonPacket( nflits = 3 )
+  pkt.flits[0][ CHIPID ] = b14(dst_chipid)
+  pkt.flits[0][ XPOS   ] = b8(dst_x)
+  pkt.flits[0][ YPOS   ] = b8(dst_y)
+  pkt.flits[0][ PLEN   ] = b8(2) 
+  pkt.flits[0][ CHIPID ] = b8(2) 
+  pkt.flits[0][ MTYPE  ] = b8(19) if cacheable else b8(14) 
+
+  pkt.flits[1][ ADDR   ] = b48(addr)
+  pkt.flits[1][ NBYTES ] = b3(nbytes)
+
+  pkt.flits[2][ CHIPID ] = b14(src_chipid)
+  pkt.flits[2][ XPOS   ] = b8(src_x)
+  pkt.flits[2][ YPOS   ] = b8(src_y)
+
+  return pkt
+
+#-------------------------------------------------------------------------
+# mk_piton_wr_req
+#-------------------------------------------------------------------------
+
+def mk_piton_wr_req( addr, nbytes, cacheable, tag, data, 
+                     dst_chipid=0, dst_x=0, dst_y=0, 
+                     src_chipid=0, src_x=0, src_y=0 ):  
+  pkt = PitonPacket( nflits = 11 )
+  pkt.flits[0][ CHIPID ] = b14(dst_chipid)
+  pkt.flits[0][ XPOS   ] = b8(dst_x)
+  pkt.flits[0][ YPOS   ] = b8(dst_y)
+  pkt.flits[0][ PLEN   ] = b8(10) 
+  pkt.flits[0][ CHIPID ] = b8(2) 
+  pkt.flits[0][ MTYPE  ] = b8( STORE_MEM ) if cacheable else b8( NC_STORE_MEM_ACK ) 
+
+  pkt.flits[1][ ADDR   ] = b48(addr)
+  pkt.flits[1][ NBYTES ] = b3(nbytes)
+
+  pkt.flits[2][ CHIPID ] = b14(src_chipid)
+  pkt.flits[2][ XPOS   ] = b8(src_x)
+  pkt.flits[2][ YPOS   ] = b8(src_y)
+
+  for i in range( 8 ):
+    pkt.flits[i+3] = data[i*64:(i+1):64]
+
+  return pkt
+
+#-------------------------------------------------------------------------
+# mk_piton_rd_resp
+#-------------------------------------------------------------------------
+
+def mk_piton_rd_resp( data, tag, chipid=0, dst_x=0, dst_y=0 ):
+  pkt = PitonPacket( nflits=9 )
+
+  pkt.flits[0][ MTYPE ] = b8(LOAD_MEM_ACK) 
+  pkt.flits[0][ PLEN  ] = b8(8)
+  pkt.flits[0][ TAG   ] = b8(tag)
+
+  for i in range( 8 ):
+    pkt.flits[i+1] = data[i*64:(i+1):64]
+
+#-------------------------------------------------------------------------
+# mk_piton_wr_resp
+#-------------------------------------------------------------------------
+
+def mk_piton_wr_resp( data, tag, chipid=0, dst_x=0, dst_y=0 ):
+  pkt = PitonPacket( nflits=9 )
+
+  pkt.flits[0][ MTYPE  ] = b8(LOAD_MEM_ACK) 
+  pkt.flits[0][ PLEN   ] = b8(0)
+  pkt.flits[0][ TAG    ] = b8(tag)
+  pkt.flits[0][ CHIPID ] = b14(dst_chipid)
+  pkt.flits[0][ XPOS   ] = b8(dst_x)
+  pkt.flits[0][ YPOS   ] = b8(dst_y)
