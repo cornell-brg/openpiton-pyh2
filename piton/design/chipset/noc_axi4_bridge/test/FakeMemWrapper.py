9f57d4715caeca8e42911d2a4c0889a228503925
yo96 <yo96@cornell.edu>
2020-01-01 22:03:20 -0500

    format

--- piton/design/chipset/noc_axi4_bridge/test/FakeMemWrapper.py
+++ piton/design/chipset/noc_axi4_bridge/test/FakeMemWrapper.py
@@ -7,18 +7,86 @@ from pytml3 import *
 
 from .fake_axi4_mem.py import fake_axi4_mem
 
+BitsNOCDataWidth = Bits64
+BitsAXI4IDWidth  = Bits6
+BitsAXI4AddrWidth = Bits64
+BitsAXI4LenWidth = Bits8
+BitsAXI4SizeWidth = Bits3
+BitsAXI4BurstWidth = Bits2
+BitsAXI4CacheWidth = Bits4
+BitsAXI4PortWidth = Bits3
+BitsAXI4QoSWidth = Bits4
+BitsAXI4RegionWidth = Bits4
+BitsAXI4UserWidth = Bits11
+BitsAXI4DataWidth = Bits512
+BitsAXI4RespWidth = Bits2
+BitsAXI4StrbWidth = Bits64
+
 class FakeMemWrapper( Component ):
 
   def construct( s ):
-    s.m_axis_awid   = InPort( BitsN )
-    s.m_axis_awaddr = InPort( BitsN )
-    ...
 
+    # Interface 
+
+    s.m_axi_awid     = InPort( BitsAXI4IDWidth )
+    s.m_axi_awaddr   = InPort( BitsAXI4AddrWidth )
+    s.m_axi_awlen    = InPort( BitsAXI4LenWidth )
+    s.m_axi_awsize   = InPort( BitsAXI4SizeWidth )
+    s.m_axi_awburst  = InPort( BitsAXI4BurstWidth )
+    s.m_axi_awlock   = InPort( Bits1 )
+    s.m_axi_awcache  = InPort( BitsAXI4CacheWidth )
+    s.m_axi_awprot   = InPort( BitsAXI4PortWidth )
+    s.m_axi_awqos    = InPort( BitsAXI4QoSWidth )
+    s.m_axi_awregion = InPort( BitsAXI4RegionWidth )
+    s.m_axi_awuser   = InPort( BitsAXI4UserWidth )
+    s.m_axi_awvalid  = InPort( Bits1 )
+    s.m_axi_awready  = OutPort( Bits1 )
+
+    s.m_axi_wid    = InPort( BitsAXI4IDWidth )
+    s.m_axi_wdata  = InPort( BitsAXI4DataWidth )
+    s.m_axi_wstrb  = InPort( BitsAXI4StrbWidth )
+    s.m_axi_wlast  = InPort( Bits1 )
+    s.m_axi_wuser  = InPort( BitsAXI4UserWidth )
+    s.m_axi_wvalid = InPort( Bits1 )
+    s.m_axi_wready = OutPort( Bits1 )
+
+    s.m_axi_arid     = InPort( BitsAXI4IDWidth )
+    s.m_axi_araddr   = InPort( BitsAXI4AddrWidth )
+    s.m_axi_arlen    = InPort( BitsAXI4LenWidth )
+    s.m_axi_arsize   = InPort( BitsAXI4SizeWidth )
+    s.m_axi_arburst  = InPort( BitsAXI4BurstWidth )
+    s.m_axi_arlock   = InPort( Bits1 )
+    s.m_axi_arcache  = InPort( BitsAXI4CacheWidth )
+    s.m_axi_arprot   = InPort( BitsAXI4PortWidth )
+    s.m_axi_arqos    = InPort( BitsAXI4QoSWidth )
+    s.m_axi_arregion = InPort( BitsAXI4RegionWidth )
+    s.m_axi_aruser   = InPort( BitsAXI4UserWidth )
+    s.m_axi_arvalid  = InPort( Bits1 )
+    s.m_axi_arready  = OutPort( Bits1 )
+
+    s.m_axi_rid    = OutPort( BitsAXI4IDWidth )
+    s.m_axi_rdata  = OutPort( BitsAXI4DataWidth )
+    s.m_axi_rresp  = OutPort( BitsAXI4RespWidth )
+    s.m_axi_rlast  = OutPort( Bits1 )
+    s.m_axi_ruser  = OutPort( BitsAXI4UserWidth )
+    s.m_axi_rvalid = OutPort( Bits1 )
+    s.m_axi_rready = InPort( Bits1 )
+
+    s.m_axi_bid    = OutPort( BitsAXI4IDWidth )
+    s.m_axi_bresp  = OutPort( BitsAXI4RespWidth )
+    s.m_axi_buser  = OutPort( BitsAXI4UserWidth )
+    s.m_axi_bvalid = OutPort( Bits1 )
+    s.m_axi_bready = InPort( Bits1 )
+
+    # Component
 
     s.fake_mem = fake_axi4_mem() 
 
+    # Update block
+
     @s.update
-    def xtick():
+    def xtick_read_addr():
+
       fake_axi4_mem.read()
 
 
--- piton/design/chipset/noc_axi4_bridge/test/fake_axi4_mem.py
+++ piton/design/chipset/noc_axi4_bridge/test/fake_axi4_mem.py
@@ -1,37 +1,37 @@
 from pymtl3 import *
 
 class axi4_read_req:
-    def __init__(self, arid : Bits6,
-                       araddr : Bits64,
-                       arlen : Bits8,
-                       arsize : Bits3,
-                       arburst : Bits2,
-                       arlock : Bits1,
-                       arcache : Bits4,
-                       arprot : Bits3,
-                       arqos : Bits4,
+    def __init__(self, arid     : Bits6,
+                       araddr   : Bits64,
+                       arlen    : Bits8,
+                       arsize   : Bits3,
+                       arburst  : Bits2,
+                       arlock   : Bits1,
+                       arcache  : Bits4,
+                       arprot   : Bits3,
+                       arqos    : Bits4,
                        arregion : Bits4,
-                       aruser : Bits11    ):
-        self.arid = arid
-        self.araddr = araddr
-        self.arlen = arlen
-        self.arsize = arsize
-        self.arburst = arburst
-        self.arlock = arlock
-        self.arcache = arcache
-        self.arprot = arprot
-        self.arqos = arqos
+                       aruser   : Bits11 ):
+        self.arid     = arid
+        self.araddr   = araddr
+        self.arlen    = arlen
+        self.arsize   = arsize
+        self.arburst  = arburst
+        self.arlock   = arlock
+        self.arcache  = arcache
+        self.arprot   = arprot
+        self.arqos    = arqos
         self.arregion = arregion
-        self.aruser = aruser
+        self.aruser   = aruser
 
 class axi4_read_resp:
     def __init__(self,
-                 rid : Bits6,
+                 rid   : Bits6,
                  rdata : Bits512,
                  rresp : Bits2,
                  rlast : Bits1,
                  ruser : Bits11 ):
-        self.rid = rid
+        self.rid   = rid
         self.rdata = rdata
         self.rresp = rresp
         self.rlast = rlast
@@ -39,45 +39,45 @@ class axi4_read_resp:
 
 class axi4_write_req:
     def __init__(self,
-                 awid : Bits6,
-                 awaddr : Bits64,
-                 awlen : Bits8,
-                 awsize : Bits3,
-                 awburst : Bits2,
-                 awlock : Bits1,
-                 awcache : Bits4,
-                 awprot : Bits3,
-                 awqos : Bits4,
+                 awid     : Bits6,
+                 awaddr   : Bits64,
+                 awlen    : Bits8,
+                 awsize   : Bits3,
+                 awburst  : Bits2,
+                 awlock   : Bits1,
+                 awcache  : Bits4,
+                 awprot   : Bits3,
+                 awqos    : Bits4,
                  awregion : Bits4,
-                 awuser : Bits11,
-                 wid : Bits6,
-                 wdata : Bits512,
-                 wstrb : Bits64,
-                 wlast : Bits1,
-                 wuser : Bits11):
-        self.awid = awid
-        self.awaddr = awaddr
-        self.awlen = awlen
-        self.awsize = awsize
-        self.awburst = awburst
-        self.awlock = awlock
-        self.awcache = awcache
-        self.awprot = awprot
-        self.awqos = awqos
+                 awuser   : Bits11,
+                 wid      : Bits6,
+                 wdata    : Bits512,
+                 wstrb    : Bits64,
+                 wlast    : Bits1,
+                 wuser    : Bits11):
+        self.awid     = awid
+        self.awaddr   = awaddr
+        self.awlen    = awlen
+        self.awsize   = awsize
+        self.awburst  = awburst
+        self.awlock   = awlock
+        self.awcache  = awcache
+        self.awprot   = awprot
+        self.awqos    = awqos
         self.awregion = awregion
-        self.awuser = awuser
-        self.wid = wid
-        self.wdata = wdata
-        self.wstrb = wstrb
-        self.wlast = wlast
-        self.wuser = wuser
+        self.awuser   = awuser
+        self.wid      = wid
+        self.wdata    = wdata
+        self.wstrb    = wstrb
+        self.wlast    = wlast
+        self.wuser    = wuser
 
 class axi4_write_resp:
     def __init__(self,
-                 bid : Bits6,
+                 bid   : Bits6,
                  bresp : Bits2,
                  buser : Bits):
-        self.bid = bid
+        self.bid   = bid
         self.bresp = bresp
         self.buser = buser
 
@@ -85,16 +85,18 @@ class fake_axi4_mem:
     def __init__(self):
         self.mem = {}
 
-    def read(self, req : axi4_read_req) -> axi4_read_resp:
+    def read( self, req : axi4_read_req ) -> axi4_read_resp:
         index = req.araddr >> 6
         return axi4_read_resp(req.arid, self.mem[index], Bits2(0), Bits1(1), req.aruser)
 
-    def write(self, req: axi4_write_req) -> axi4_write_resp:
+    def write( self, req: axi4_write_req ) -> axi4_write_resp:
         index = req.awaddr >> 6
         write_data = self.mem[index]
+
         for i in range(64):
             if req.wstrb[i] == Bits1(1):
                 write_data[i*8:i*8+63] = req.wdata[i*8:i*8+63]
+
         self.mem[index] = write_data
-        return axi4_write_resp(req.awid, Bits2(0), req.buser)
+        return axi4_write_resp( req.awid, Bits2(0), req.buser)
 
